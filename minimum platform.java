8eaa701c6eead83c9d475fdc63dd169b4425c1bf
hitesh dhayal <89539262+hiteshdhayall@users.noreply.github.com>
2022-05-24 22:59:57 +0530

    Update minimum platform.java

--- minimum platform.java
+++ minimum platform.java
@@ -1,1 +1,25 @@
-
+	// Your code here
+    	sort(a,a+n);
+    	sort(b,b+n);
+    	int max=1;
+    	int req=1;
+    	
+    	int i=1;
+    	int j=0;
+    	while(i<n && j<n){
+    	    if(a[i]<=b[j]){
+    	    max++;
+    	    i++;
+    	    }
+    	    else{
+    	    max--;
+    	    j++;}
+    	    
+    	    if(max>req)
+    	    req=max;
+    	    
+    	    
+    	    
+    	}
+    	
+    return req;	
